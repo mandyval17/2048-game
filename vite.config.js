e4ee7c99b865ed3242aa2b4bbea5166ed0791c59
mandyval17 <mayurvalecha2603@gmail.com>
2025-01-16 18:54:59 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  alias: {
+    '@mui/material': '@mui/material',
+    '@mui/icons-material': '@mui/icons-material',
+  },
 })
